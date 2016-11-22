00c6a10858553893906e66376ab558117cde1dd3
Gavin <gavin.kelly@crick.ac.uk>
2016-11-22 13:53:39 +0000

    Initial template structure

---
+++ analyse.r
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(devtools)
+if (grepl("thecrick", Sys.info()['nodename'])) {
+  load_all(Sys.getenv("my_r_package"))
+} else {
+  install_github("macroscian/R-Package")
+}
+
+params <- params_init(title="",
+                      script="analyse.r",
+                      alpha=0.05,
+                      seed=1,
+                      author="gavin.kelly")
+
+
+set.seed(seed=params("seed"))
+dirs <- derivedDirs()
+
+
+################################################################
+#### Do Analysis
+################################################################
+
+    
+
+
+################################################################
+#### Output Results
+################################################################
+
+dframe <- t(as.data.frame(params()))
+fname <- sprintf("%s/results_%s.xls", dirs$results,  params()$fVersion)
+write.xlsx(dframe, file=fname, sheetName="Parameters", col.names=FALSE)
